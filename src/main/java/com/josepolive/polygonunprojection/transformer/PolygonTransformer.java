f58abffae37f7edb64598387c420052632f1243f
JosepOli <josepolive89@gmail.com>
2024-03-03 12:22:48 +0100

    Transformation classes created

--- src/main/java/com/josepolive/polygonunprojection/transformer/PolygonTransformer.java
+++ src/main/java/com/josepolive/polygonunprojection/transformer/PolygonTransformer.java
@@ -1,4 +1,18 @@
 package com.josepolive.polygonunprojection.transformer;
 
+import com.josepolive.polygonunprojection.model.Point2D;
+import com.josepolive.polygonunprojection.model.Polygon;
+import com.josepolive.polygonunprojection.model.Polygon3D;
+import com.josepolive.polygonunprojection.util.TransformationUtil;
+
 public class PolygonTransformer {
+
+    public Polygon3D transform(Polygon polygon, double slopeDegrees, double azimuthDegrees) {
+        Polygon3D polygon3D = new Polygon3D();
+
+        for (Point2D vertex : polygon.getVertices()) {
+            polygon3D.addVertex(TransformationUtil.transformPoint(vertex, slopeDegrees, azimuthDegrees));
+        }
+        return polygon3D;
+    }
 }
--- src/main/java/com/josepolive/polygonunprojection/util/TransformationUtil.java
+++ src/main/java/com/josepolive/polygonunprojection/util/TransformationUtil.java
@@ -1,4 +1,21 @@
 package com.josepolive.polygonunprojection.util;
 
+import com.josepolive.polygonunprojection.model.Point2D;
+import com.josepolive.polygonunprojection.model.Point3D;
+
 public class TransformationUtil {
+
+    public static Point3D transformPoint(Point2D point2D, double slopeDegrees, double azimuthDegrees) {
+
+        //We convert degrees to radians for mathematical operations
+        double slopeRadians = Math.toRadians(slopeDegrees);
+        double azimuthRadians = Math.toRadians(azimuthDegrees);
+
+        double z = point2D.getY() * Math.tan(slopeRadians);
+
+        double x = (point2D.getX() * Math.cos(azimuthRadians)) - (point2D.getY() * Math.sin(azimuthRadians));
+        double y = (point2D.getX() * Math.sin(azimuthRadians)) + (point2D.getY() * Math.cos(azimuthRadians));
+
+        return new Point3D(x, y, z);
+    }
 }
